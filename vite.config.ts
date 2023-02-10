41659b382c5e3214609feb2371dd648292b51c47
Michael Salaverry <barakplasma@gmail.com>
2023-02-10 21:28:06 +0200

    Fix: Custom domain

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,6 @@ import vueJsx from "@vitejs/plugin-vue-jsx";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/bakeapizza/",
   plugins: [vue(), vueJsx()],
   resolve: {
     alias: {
